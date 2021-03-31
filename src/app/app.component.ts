680214fb7ef9eaf1ff271de8a9250f8ae067ffa0
ChangaHettiarachchi <subanipathirana98@gmail.com>
2021-03-31 09:19:18 +0530

    First

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'MyAppCalculator';
+  title = 'MyAppCalculator Tutorial Swift Learn!';
+
+  public num1:number;
+  public num2:number;
+  public num3:number;
+
+  add(){
+    this.num3=this.num1+this.num2;
+  }
+
+  sub(){
+    this.num3=this.num1-this.num2;
+  }
+
+  multiply(){
+    this.num3=this.num1*this.num2;
+  }
+
+  divide(){
+    this.num3=this.num1/this.num2;
+  }
 }
